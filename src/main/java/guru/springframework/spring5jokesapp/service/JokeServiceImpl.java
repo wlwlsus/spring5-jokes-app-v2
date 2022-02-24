a78571b0a3f98d0a78ba798799132b3e1e998754
wlwlsus <159632_@naver.com>
2022-02-24 13:35:10 +0900

    [add]custom banner

--- src/main/java/guru/springframework/spring5jokesapp/service/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesapp/service/JokeServiceImpl.java
@@ -9,7 +9,7 @@ public class JokeServiceImpl implements JokeService {
   private final ChuckNorrisQuotes chuckNorrisQuotes;
 
   public JokeServiceImpl() {
-    this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    this.chuckNorrisQuotes = new ChuckNorrisQuotes( );
   }
 
   @Override
